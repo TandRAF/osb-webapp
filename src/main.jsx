9dc6091960e95a68b611ccacba8e6b16f616f7e8
Rafael <83817988+TandRAF@users.noreply.github.com>
2024-05-02 00:08:47 +0300

    added basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import "./SCSS/Index.scss";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/osb-webapp'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
