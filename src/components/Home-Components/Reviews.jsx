a0161027c0a3a9192e3396467a5a9a2b9b795da0
Rafael <83817988+TandRAF@users.noreply.github.com>
2024-05-02 16:45:51 +0300

    review responsive update

--- src/components/Home-Components/Reviews.jsx
+++ src/components/Home-Components/Reviews.jsx
@@ -3,29 +3,29 @@ import { Stars,Quotes } from "../../assets/Icons/Icons"
 import { reviewItems } from "../../assets/content"
 import { useState,useEffect } from "react"
 
-// const showCards = (items) => {
-//     return(
-//         <>
-//             {items.map((item,index)=>{
-//             return(
-//                 <div key={index} className="card">
-//                     <Quotes/>
-//                     <p>{item.text}</p>
-//                     <div className="card-name-status">
-//                         <div style={{ backgroundImage: `url("src/assets/Images/${item.img}")` }} className="card-img">
+const showCards = (items) => {
+    return(
+        <>
+            {items.map((item,index)=>{
+            return(
+                <div key={index} className="card">
+                    <Quotes/>
+                    <p>{item.text}</p>
+                    <div className="card-name-status">
+                        <div style={{ backgroundImage: `url("src/assets/Images/${item.img}")` }} className="card-img">
     
-//                         </div>
-//                         <div className="name-status">
-//                             <span>{item.name}</span>
-//                             <span>{item.status}</span>
-//                         </div>
-//                     </div>
-//                 </div>
-//             )
-//             })}
-//         </>
-//     )
-// }
+                        </div>
+                        <div className="name-status">
+                            <span>{item.name}</span>
+                            <span>{item.status}</span>
+                        </div>
+                    </div>
+                </div>
+            )
+            })}
+        </>
+    )
+}
 
 export const Reviews = () => {
     const [width, setWidth] = useState(window.innerWidth);
@@ -40,23 +40,13 @@ export const Reviews = () => {
                 <h2>Ce spun <b>membrii</b> noștri ?</h2>
                 <p>Părerile și experiențele membrilor noștri sunt fundamentale pentru noi.  Iată ce au de spus despre implicarea lor în comunitatea noastră.  Feedback-ul lor ne inspiră și ne motivează să continuăm să creștem și să  dezvoltăm proiectele noastre, pentru a aduce beneficii reale membrilor  noștri și comunității în ansamblu.</p>
             </div>
-            {reviewItems.map((item,index)=>{
-                return(
-                    <div key={index} className="card">
-                        <Quotes/>
-                        <p>{item.text}</p>
-                        <div className="card-name-status">
-                            <div style={{ backgroundImage: `url("src/assets/Images/${item.img}")` }} className="card-img">
-
-                            </div>
-                            <div className="name-status">
-                                <span>{item.name}</span>
-                                <span>{item.status}</span>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
+            {(width > 768) ? <>{showCards(reviewItems)}</>:<> 
+            <div className="card-box" >
+                <div className="box">
+                    {showCards(reviewItems)}
+                </div>
+            </div>
+            </> }
         </section>
     </>
   )
